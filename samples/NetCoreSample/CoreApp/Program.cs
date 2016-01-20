5c0a264bc0f82d609b514864a92b4704d6fc5de8
Wes Haggard <Wes.Haggard@microsoft.com>
2016-01-20 11:55:20 -0800

    Get a Azure networking code sample working with packages.
    Note that System.Net.* libaries are actually in Microsoft.NETCore.Console
    meta-package but I listed them explicitly so that others know the versions
    included.
        "System.Net.Primitives": "4.0.11-rc2-23712",
        "System.Net.Security": "4.0.0-rc2-23712",
        "System.Net.Sockets": "4.1.0-rc2-23712",

--- samples/NetCoreSample/CoreApp/Program.cs
+++ samples/NetCoreSample/CoreApp/Program.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
 using System.Text;
+
 
 namespace coreApp
 {
@@ -9,9 +14,31 @@ namespace coreApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"{NetCoreLibrary.Class1.SayHello()} world");
-            Console.WriteLine("Press enter to continue");
-            Console.ReadLine();
+            Uri address = new Uri("https://www.msn.com:443");
+            IPAddress ipv4Address = Dns.GetHostAddressesAsync(address.Host).Result.First(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+
+            using (Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp))
+            {
+                socket.Connect(ipv4Address, address.Port);
+                using (NetworkStream networkStream = new NetworkStream(socket, false))
+                using (SslStream sslStream = new SslStream(networkStream))
+                {
+                    sslStream.AuthenticateAsClientAsync(address.Host).Wait();
+                    StreamWriter requestWriter = new StreamWriter(sslStream, Encoding.ASCII, 4096, true);
+                    requestWriter.Write("GET https://" + address.Host + "/ HTTP/1.1\r\n");
+                    requestWriter.Write("Host: " + address.Host + "\r\n");
+                    requestWriter.Write("\r\n");
+                    requestWriter.Write("\r\n");
+                    requestWriter.Flush();
+
+                    StreamReader responseReader = new StreamReader(sslStream, Encoding.ASCII, false, 4096, true);
+                    string responseLine;
+                    while (!string.IsNullOrEmpty((responseLine = responseReader.ReadLine())))
+                    {
+                        Console.WriteLine(responseLine);
+                    }
+                }
+            }
         }
     }
 }
